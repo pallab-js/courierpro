d838a6e31d31cee8454d36cbfbd951dd4be8dfea
pallab-js <sonowalpallabjyoti@gmail.com>
2026-07-06 22:26:47 +0530

    Initial Commit

---
+++ courierpro/courierproApp.swift
@@ -0,0 +1,17 @@
+//
+//  courierproApp.swift
+//  courierpro
+//
+//  Created by Pallab Jyoti Sonowal on 06/07/26.
+//
+
+import SwiftUI
+
+@main
+struct courierproApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
